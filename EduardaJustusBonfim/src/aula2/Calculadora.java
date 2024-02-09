d57e6b0042a6cb42793e8d58d5dfd4ddd7f43306
justus <130786194+eduardajustus@users.noreply.github.com>
2024-02-09 17:28:01 -0300

    Add files via upload

---
+++ EduardaJustusBonfim/src/aula2/Calculadora.java
@@ -0,0 +1,11 @@
+package aula2;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+        int soma = x + y;
+        System.out.println(soma);
+        
+    }
+}
---
+++ EduardaJustusBonfim/src/aula2/HelloWorld.java
@@ -0,0 +1,9 @@
+package aula2;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String meuTexto = "Meu Texto!" ;
+        System.out.println(meuTexto);
+        System.out.println("Hello World");  
+    }
+}
